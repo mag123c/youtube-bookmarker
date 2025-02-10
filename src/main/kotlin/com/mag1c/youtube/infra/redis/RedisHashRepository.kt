91b831a4fff60c82c7a15c6f841bc325da68b21b
mag123c <diehreo@gmail.com>
2025-02-11 00:08:19 +0900

    test: redis i/o integration

--- src/main/kotlin/com/mag1c/youtube/infra/redis/RedisHashRepository.kt
+++ src/main/kotlin/com/mag1c/youtube/infra/redis/RedisHashRepository.kt
@@ -1,8 +1,8 @@
 package com.mag1c.youtube.infra.redis
 
-interface RedisHashRepository.kt`<K, HK, HV> {
+interface RedisHashRepository<K, HK, HV> {
     fun put(key: K, hashKey: HK, value: HV) //HSET
-    fun get(key: K, hashKey: HK) // HGET
+    fun get(key: K, hashKey: HK): HV? // HGET
     fun delete(key: K, hashKey: HK) //HDEL
     fun findAll(key: K): Map<HK, HV>
 }
